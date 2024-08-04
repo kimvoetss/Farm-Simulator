47b71940088ef5789277096283dd7797305dc62d
kimvoetss <kimvoets2004@gmail.com>
2024-08-04 17:52:42 +0200

    Interact Farmland

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Movement Components
+    private CharacterController controller;
+    private Animator animator;
+
+    private float moveSpeed = 4f;
+
+    [Header("Movement System")]
+    public float walkSpeed = 4f;
+    public float runSpeed = 8f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get movement components
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Runs the function that handles all movement
+        Move();
+    }
+
+
+    public void Move()
+    {
+        //Get the horizontal and vertical inputs as a number
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        //Direction in a normalised vector
+        Vector3 dir = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 velocity = moveSpeed * Time.deltaTime * dir;
+
+        //Is the sprint key pressed down?
+        if (Input.GetButton("Sprint"))
+        {
+            //Set the animation to run and increase our movespeed
+            moveSpeed = runSpeed;
+            animator.SetBool("Running", true);
+        }
+        else
+        {
+            //Set the animation to walk and decrease our movespeed
+            moveSpeed = walkSpeed;
+            animator.SetBool("Running", false);
+        }
+
+
+        //Check if there is movement
+        if (dir.magnitude >= 0.1f)
+        {
+            //Look towards that direction
+            transform.rotation = Quaternion.LookRotation(dir);
+
+            //Move
+            controller.Move(velocity);
+
+        }
+
+        //Animation speed parameter
+        animator.SetFloat("Speed", velocity.magnitude);
+
+
+
+    }
+}
---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+  
+    //The land the player is currently selecting
+    Land selectedLand = null; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit; 
+        if(Physics.Raycast(transform.position, Vector3.down,out hit,  1))
+        {
+            OnInteractableHit(hit);
+        }
+    }
+
+    //Handles what happens when the interaction raycast hits something interactable
+    void OnInteractableHit(RaycastHit hit)
+    {
+        Collider other = hit.collider;
+        
+        //Check if the player is going to interact with land
+        if(other.tag == "Land")
+        {
+            //Get the land component
+            Land land = other.GetComponent<Land>();
+            SelectLand(land);
+            return; 
+        }
+
+        //Deselect the land if the player is not standing on any land at the moment
+        if(selectedLand != null)
+        {
+            selectedLand.Select(false);
+            selectedLand = null;
+        }
+    }
+
+    //Handles the selection process of the land
+    void SelectLand(Land land)
+    {
+        //Set the previously selected land to false (If any)
+        if (selectedLand != null)
+        {
+            selectedLand.Select(false);
+        }
+        
+        //Set the new selected land to the land we're selecting now. 
+        selectedLand = land; 
+        land.Select(true);
+    }
+
+
+}
