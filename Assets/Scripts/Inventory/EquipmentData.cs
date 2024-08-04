f18bca7b3469e77fa1da1432941e7019de7c539c
kimvoetss <kimvoets2004@gmail.com>
2024-08-04 20:31:33 +0200

    Inventory + interact with soil

---
+++ Assets/Scripts/Inventory/EquipmentData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/Equipment")]
+public class EquipmentData : ItemData
+{
+    public enum ToolType
+    {
+        Hoe, WateringCan, Axe, Pickaxe
+    }
+    public ToolType toolType;
+}
