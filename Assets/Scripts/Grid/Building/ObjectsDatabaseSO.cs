11607030c9c150180f72fae9f8372c74bf371c11
kimvoetss <kimvoets2004@gmail.com>
2024-08-13 13:35:01 +0200

    Fix?

---
+++ Assets/Scripts/Grid/Building/ObjectsDatabaseSO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ObjectsDatabaseSO : ScriptableObject
+{
+    public List<ObjectData> objectData;
+}
+
+public class ObjectData
+{
+
+}
